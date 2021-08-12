0b6f556a4b78fdcf5c3e81be6dce6f692d7080ca
RaisaRamazanova <ramazanova.raisat@yandex.ru>
2021-08-12 23:37:53 +0300

    Initial Commit

---
+++ TestPryaniky/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestPryaniky
+//
+//  Created by Раисат Рамазанова on 12.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
